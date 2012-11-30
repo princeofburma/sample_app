6168ffa1e8e68d5f887a2a544d0c03acf2132ef1
princeofburma <drosemill@gmail.com>
2012-11-30 18:01:58 -0500

    5.3.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  match 'static_pages/home',   to: 'static_pages#home'
+  match 'static/home',   to: 'static_pages#home'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
